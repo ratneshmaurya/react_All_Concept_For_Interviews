e60886880cf27e38f1f4d655354462f5e32a6037
Ratnesh Maurya <ratmaury1@publicisgroupe.net>
2026-05-14 20:11:54 +0530

    updated closure and stale closure concept used in react

--- 000_closures_VS_StaleClosure_in_React.jsx
+++ 000_closures_VS_StaleClosure_in_React.jsx
@@ -1,6 +1,6 @@
-//  Topics :
+// Topics :
 // React me closure kya hota?
-// Stale closure kya hota?
+// Stale closure kya hota and deep functionality?
 
 
 // Before anything remeber one thing: React me har render pe, poora component function firse create hota hai, 
@@ -118,8 +118,8 @@ setCount(2)
 // -------------------------------------------------------------------------------
 // --------------------------------- STALE CLOSURE ---------------------------------
 // Stale closure: when a function remembers old value, and not the updated value, then we call it stale closure.
-// Closure problem tab hoti hai when:
-// koi function future me execute ho AND wo old render ke values remember kare
+// **** Closure problem tab hoti hai when: koi function future me execute ho AND wo old render ke values remember kare****
+// So Stale-closure => When asyn operation is inside a function and that asyn call executes later.
 
 // ⚡ Isliye stale closures commonly: asyn calls.
 // setTimeout
@@ -128,8 +128,10 @@ setCount(2)
 // promises
 // async callbacks
 // me dikhti hain.
+
 // Becoz, when they moved to callback queue(as they all are asyn operations) they take the old closure 
-// value jo uss point of time me thi, and work on that value only
+// value jo uss point of time me thi, and work on that value only till that asyn callback executes.
+// We does not need useEffect for stale closure, just need any asyn call inside any function, that re-renders.
 
 
 // Same example me hi, using useEffect and setInterval(so that we can see the stale values in console
@@ -141,7 +143,9 @@ function Counter() {
   // stale closure will happen here, becoz this function will execute later after sometime, not immediately.
   // Means, when it runs first time, it moves to callback queue and then there on every one sec
   // it will execute and print the older value, becoz it has taken the older closure value and moved out.
+
   useEffect(() => {
+    // setInterval hai(not setTimeout), so it will keep on running in callback queue.
     setInterval(() => {
       console.log("interval:", count)
     }, 1000)
@@ -167,7 +171,7 @@ setInterval(() => {
 
 // This interval callback remembers: count=0
 
-// Ab click krne pe:
+// Ab 2-3 baar click krne pe:
 // Count becomes:
 // 1
 // 2
@@ -190,6 +194,8 @@ setInterval(() => {
 
 // Meaning
 // old function old values pakad ke baitha hai
+// Aur agr useEffect nhi lgate, toh bhi stale closure hota, but extra cheej ye hoti ki, hrr re-render pe new interval 
+// extra create hote, and wo bhi callback queue me chlte rhte, apne time ki count-values ko pakadkrr.
 
 // ----------------------------  FIX, adding depedency in useEffect
 useEffect(() => {
@@ -209,8 +215,19 @@ useEffect(() => {
 
 
 // -------------- what if we dont use clearInterval ???
-//  so hrr interval jo created hoga, wo apna apna kaam karta rahega, and print karta rahega, 
+// so hrr interval jo created hoga, wo apna apna kaam karta rahega, and print karta rahega, 
 // and it will create a mess in console, and also memory leak.
+
+// i.e WITHOUT cleanup:
+// multiple intervals
+// multiple async callbacks
+// each with own closure
+
+// So: on each re-render, create new interval.
+// Interval #1 → remembers 0, and keeps printing 0 in interval of 1sec
+// Interval #2 → remembers 1, and keeps printing 1 in interval of 1sec
+// Interval #3 → remembers 2, and keeps printing 2 in interval of 1sec
+// ALL running independently 😵
 
 // -------------------------- Clear Interval concept 
 // Generally:
@@ -236,3 +253,46 @@ useEffect(() => {
 // --------------- setTimeout me?
 // Less dangerous because: ek hi baar run hota
 // Still cleanup better practice hai.
+
+
+// ---------------------------- Extra learning :
+//  here two buttons
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setTimeout(() => {
+      console.log(count)
+    }, 3000)
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Log after 3 sec
+      </button>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </>
+  )
+}
+
+// This case is not stale closure : (first click on Increase button, then click on Log after 3 sec button)
+// yha agr phle mai Increase button pe click krunga toh count=1 hoga and react-renders.
+// and handleClick function firse create hoga, and uska closure ab count=1 ko remember karega, 
+// and fir jab mai Log after 3 sec pe click krunga toh wo handleClick function execute hoga, and 
+// uska closure me count=1 hoga, so output: 1
+
+// This case stale closure: (first click on Log after 3 sec, then click on Increase button)
+// Yha agr phle mai Log after 3 sec pe click krunga toh wo handleClick function execute hoga, and 
+// uska closure me count=0 hoga, and wo callback queue me gya, for 3 sec before executing.
+// but in meantime before 3 sec, I pressed Increase button, and count=1 ho gaya, and react-renders, 
+// and new handleClick function banta hai, but wo pehle wala handleClick function jo count=0 ko remember 
+// karta hai, wo stale closure ban jata hai, and jab wo execute hota hai after 3 sec, toh output: 0 hi rhta hai
+// becoz wo callback queue me tha.
+
+//  So always remember agr, callback queue me koi function gaya hai, toh wo stale closure ban sakta hai, 
+// becoz wo old closure value ko pakad ke baitha hai, and jab wo execute hoga, toh wo old value ko hi use 
+// karega, even if the state value is updated in meantime.
